f50867b6e42e9e1c486149c8b194b289c18c946d
Souvik Biswas <you@example.com>
2023-02-18 19:39:30 +0530

    spring starter project created,Dependencies added(spring web,elasticsearch,devtools,validation,lombok,tomcat jasper,jakarta.json-api)

---
+++ StudentsReportingApplication/src/main/java/com/salesken/StudentsReportingApplication.java
@@ -0,0 +1,13 @@
+package com.salesken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsReportingApplication.class, args);
+	}
+
+}
